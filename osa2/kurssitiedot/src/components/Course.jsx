cf524a126bdd68f06bd65196078ec8e05fc0afa0
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2024-01-26 16:26:12 +0200

    Teht.2.5

--- osa2/kurssitiedot/src/components/Course.jsx
+++ osa2/kurssitiedot/src/components/Course.jsx
@@ -1,16 +1,65 @@
-//import Header from './Header'
+//import Header from './Header'  / kurssin alikomponentin voi olla samassa moduulissa.
 
-const Course = ({ course }) => {
-    console.log(course)
+const Course = (props) => {
+    console.log('propsit nyt', props)
+    const { course } = props
     return (
-    <div>
-      <h1>{course.name}</h1>
-      <p>{course.parts[0].name}</p>
-    </div>
+      <div>
+      <Header course = {course.name}/>
+      <Content parts ={course.parts}/>
+      <Total parts = {course.parts}/>
+      </div>
     )
   }
   
+  
+  const Header = ({course} ) => {
+    console.log('header', course)
+    return (
+      <div>
+      <h2>
+        {course}
+      </h2>
+      </div>
+    )}
+  
+  const Total = ({ parts} ) =>{
+    console.log('summa', parts) 
+  
+    //const initialValue = 0   //ei tarvita, https://medium.com/yavar/how-to-use-the-reduce-in-javascript-and-react-4bc8b5f8fa4b
+    
+    var totalExercises = parts.reduce((previousValue, currentValue) => {
+      return previousValue+currentValue.exercises;},0)
+  
+      return(
+      <div>
+      <p><b>total of {totalExercises} exercises</b></p>
+      </div>
+    )}
+  
+  
+  
+  const Part = ( props) => {
+     console.log('part', props) 
+     const { part, exercises } = props
+     return (
+      <div>
+      <p>
+      {part} {exercises}
+      </p>
+      </div>
+     )}
+  
+  
+  const Content = ({parts}) => {
+    console.log('content', parts)
+    return (
+    <div>
+      {parts.map(part =>
+        <Part key={part.id} part={part.name} exercises={part.exercises}/>)}
+        
+    </div>
+     )
+     } 
+  
   export default Course
-
-  //  <p>{course.parts[0].name}</p>
-  //<Header course={course.parts} /> 
