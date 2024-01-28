8dc56f2fc60f6323f4290fa0b79f53254008359a
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2024-01-28 12:49:45 +0200

    Teht. 2.7, step2

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -19,17 +19,27 @@ const App = () => {
   const [newName, setNewName] = useState('')
 
   const addName = (event) => {
+    if (persons.find(person =>  person.name === newName)) {
+    //if (persons.includes(newName)) {  // === newName){
+    //if (persons.indexOf(newName)!= -1){ 
+      console.log('oli jo') 
+      alert(`${newName} is already added to phonebook`);
+    }
+    else {
     event.preventDefault()
     console.log('button clicked', event.target)
-    const nameObject = {                     // tämä ao. pätkä lisää uuden noten listaan
-      name: newName
+      const nameObject = {                     // tämä ao. pätkä lisää uuden noten listaan
+        name: newName
       //,important: Math.random() > 0.5,
       //,id: persons.name
+      };
+     setPersons(persons.concat(nameObject));    // huom! concat, koska ei saa muuttaa tilaa suoraan!!!
+      setNewName('')
+      //console.log('button clicked', event.target)
     }
-  
-    setPersons(persons.concat(nameObject))    // huom! concat, koska ei saa muuttaa tilaa suoraan!!!
-    setNewName('')
-    }
+    //event.preventDefault()   //pitääkö olla täällä, toimii kyllä vanhalla paikallakin
+  };
+
 
   const handleNameChange = (event) => {
       console.log(event.target.value)
