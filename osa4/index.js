0078e21ea6f81303446ccee1b0be6b1fe42e401f
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2024-02-27 17:48:25 +0200

    Import config & logger

--- osa4/index.js
+++ osa4/index.js
@@ -4,6 +4,11 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+//const app = require('./app') // varsinainen Express-sovellus  tämä pitää jäädä
+
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
 const blogSchema = mongoose.Schema({
   title: String,
   author: String,
@@ -17,7 +22,7 @@ const Blog = mongoose.model('Blog', blogSchema)
 //const mongoUrl = `mongodb+srv://fullstack:${password}@cluster0.5lrs0ck.mongodb.net/noteApp?retryWrites=true&w=majority`
 //'mongodb://localhost/bloglist'
 
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = config.MONGODB_URI   //process.env.
 mongoose.connect(mongoUrl)
 
 app.use(cors())
@@ -43,6 +48,11 @@ app.post('/api/blogs', (request, response) => {
 
 
 //const PORT = 3003
-const PORT = process.env.PORT
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+
+//const PORT = process.env.PORT
+//app.listen(PORT)
+//console.log(`Server running on port ${PORT}`)
+
+app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
+  })
