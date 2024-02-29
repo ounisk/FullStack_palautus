1b7f5c77cb3c9721cc381fa6ff5cd00a14433ef4
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2024-02-29 19:06:55 +0200

    Teht. 4.8, step1

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,16 +1,17 @@
 const blogsRouter = require('express').Router()   // kaikki blogeihin liittyvät routet
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {      // huom! ei siis enää tarvitse /api/blogs
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
+blogsRouter.get('/', async (request, response) => {      // huom! ei siis enää tarvitse /api/blogs
+    //Blog
+    //  .find({})
+    //  .then(blogs => {
+    //    response.json(blogs)
+    const blogs = await Blog.find({})       // tässä refakotoitu promiseista async/await:iin
+    response.json(blogs) 
+    })
 
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', (request, response) => {     // 4.10 muista muuttaa tämän async/await muototon
 const blog = new Blog(request.body)
 
     blog
