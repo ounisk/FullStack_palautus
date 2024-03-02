1fd194c9e701afed483857dfe78e36b1cf9b65aa
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2024-03-02 18:06:08 +0200

    Teht.4.13, step1 (and add describe)

--- osa4/tests/blogs_api.test.js
+++ osa4/tests/blogs_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const Blog = require('../models/blog')
 
 const mongoose = require('mongoose')
@@ -45,87 +45,107 @@ test('there is field id, not _id', async () => {
     assert.notEqual(response.body[0].id, undefined)
 })
 
-
-test('a new blog can be added ', async () => {
-    const newBlog = {
-        "title": "Norge",
-        "author": "Heikki Heikalainen",
-        "url": "www.heika.fi",
-        "likes": 40,
-      }
-  
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-  
-    const response = await api.get('/api/blogs')
-  
-    const contents = response.body.map(r => r.title)
-  
-    assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)  // helper lisätty
-  
-    assert(contents.includes('Norge'))
-  })
-
-
-  test('if blog has no likes, default is 0', async () => {
-    const newBlog = {
-        "title": "Denmark",
-        "author": "Vikke Viikinki",
-        "url": "www.viikinki.fi",
-      }
-  
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-  
-    const blogsAfter = await helper.blogsInDb()
-    //const response = await api.get('/api/blogs')
-    const contents = blogsAfter.map(r => r.title)  
-
-    assert.strictEqual(blogsAfter.length, helper.initialBlogs.length + 1)  // helper lisätty
-    //logger.info('blogsAfter[2] likes', blogsAfter[2].likes)
-    assert.strictEqual(blogsAfter[2].likes, 0)
-    //assert(contents.includes('Denmark'))
-  })
-
-
-test('error if blog without title', async () => {
-    const newBlog = {
-        "author": "Zeus Zeus",
-        "url": "www.zeus.fi",
-        "likes": 50,
-    }
+describe('addition of a new blog', () => {
+    test('a new blog can be added ', async () => {
+        const newBlog = {
+            "title": "Norge",
+            "author": "Heikki Heikalainen",
+            "url": "www.heika.fi",
+            "likes": 40,
+        }
     
-    await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-    //const blogsAfter = await helper.blogsInDb()
-    //assert.strictEqual(blogsAfter.length, helper.initialBlogs.length)
+        await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    
+        const response = await api.get('/api/blogs')
+    
+        const contents = response.body.map(r => r.title)
+    
+        assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)  // helper lisätty
+    
+        assert(contents.includes('Norge'))
     })
 
 
-test('error if blog without url', async () => {
-    const newBlog = {
-        "title": "Kreikka",
-        "author": "Mytilus Myrtille",
-        "likes": 99,
-    }
+    test('if blog has no likes, default is 0', async () => {
+        const newBlog = {
+            "title": "Denmark",
+            "author": "Vikke Viikinki",
+            "url": "www.viikinki.fi",
+        }
     
-    await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+        await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    
+        const blogsAfter = await helper.blogsInDb()
+        //const response = await api.get('/api/blogs')
+        const contents = blogsAfter.map(r => r.title)  
 
-    //const blogsAfter = await helper.blogsInDb()
-    //assert.strictEqual(blogsAfter.length, helper.initialBlogs.length)
+        assert.strictEqual(blogsAfter.length, helper.initialBlogs.length + 1)  // helper lisätty
+        //logger.info('blogsAfter[2] likes', blogsAfter[2].likes)
+        assert.strictEqual(blogsAfter[2].likes, 0)
+        //assert(contents.includes('Denmark'))
     })
+
+
+    test('error if blog without title', async () => {
+        const newBlog = {
+            "author": "Zeus Zeus",
+            "url": "www.zeus.fi",
+            "likes": 50,
+        }
+        
+        await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+
+        //const blogsAfter = await helper.blogsInDb()
+        //assert.strictEqual(blogsAfter.length, helper.initialBlogs.length)
+        })
+
+
+    test('error if blog without url', async () => {
+        const newBlog = {
+            "title": "Kreikka",
+            "author": "Mytilus Myrtille",
+            "likes": 99,
+        }
+        
+        await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+
+        //const blogsAfter = await helper.blogsInDb()
+        //assert.strictEqual(blogsAfter.length, helper.initialBlogs.length)
+        })
+    }) 
+
+describe ('deletion and modification of a blog', () =>{
+    test('a blog can be deleted', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const blogToDelete = blogsAtStart[0]
+        //console.log('deletoitava', blogToDelete)
+        await api
+            .delete(`/api/blogs/${blogToDelete.id}`)
+            .expect(204)
+        const blogsAtEnd = await helper.blogsInDb()
+      
+        const contents = blogsAtEnd.map(r => r.title)
+        assert(!contents.includes(blogToDelete.title))
+        //console.log('deletoinnin jälkeen eka blogi', contents[0])
+      
+        assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+      })
+    })
+
 
 
 
