c260db869b7854ca7db37905dd167bb34d1c1a7a
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2024-03-02 11:18:32 +0200

    Create test_helper.js

---
+++ osa4/tests/test_helper.js
@@ -0,0 +1,28 @@
+const Blog =require('../models/blog')
+
+
+const initialBlogs = [
+    {
+        "title": "Finland",
+        "author": "Matti Meikalainen",
+        "url": "www.meika.fi",
+        "likes": 20
+      },
+      {
+        "title": "Sverige",
+        "author": "Tiina Teikalainen",
+        "url": "www.teika.fi",
+        "likes": 30,
+      }
+]
+
+
+const blogsInDb = async () => {
+    const blogs = await Blog.find({})
+    return blogs.map(blog => blog.toJSON())
+  }
+
+
+  module.exports = {
+    initialBlogs,blogsInDb
+  }
