9063852b8fd4769f83f4c9fda4d3f1f377116094
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2024-03-06 18:05:30 +0200

    Teht. 4.20, step8

--- osa4/app.js
+++ osa4/app.js
@@ -28,6 +28,7 @@ app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)   // jos alkuosa on /api/blogs, niin käyttää tota blogsRouteria
 app.use('/api/users', usersRouter)    // lisätty kohdassa 4c)
