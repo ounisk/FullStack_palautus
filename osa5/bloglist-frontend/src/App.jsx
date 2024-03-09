6a8a6228078fdd77a94ee84c7ba8c27f6d5391a3
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2024-03-09 17:24:36 +0200

    Teht. 5.1, step1

---
+++ osa5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,112 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+
+  //useEffect(() => {    // ei vielä 5.1
+  //    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+  //    if (loggedUserJSON) {
+  //        const user = JSON.parse(loggedUserJSON)
+  //        setUser(user)
+  //        blogService.setToken(user.token)
+  //      }
+  //    }, [])
+
+
+  const handleLogin =  async (event) => {
+      event.preventDefault()
+      console.log('logging in with', username, password)
+
+      try {
+        const user = await loginService.login({
+            username, password,
+        })
+
+        //window.localStorage.setItem(
+        //  'loggedBlogappUser', JSON.stringify(user)    
+        //)  
+        
+        //blogService.setToken(user.token)
+        setUser(user)
+        setUsername('')
+        setPassword('')
+      } catch (exception) {
+          setErrorMessage('wrong credentials')
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
+      }
+
+    }
+
+
+  const loginForm = () => (
+  
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+
+  //const noteForm = () => (                  ///huom! muuta kaikki NOte kohdat vastaamaan blogia?...
+  //  <form onSubmit={addNote}>
+  //    <input
+  //      value={newNote}
+  //      onChange={handleNoteChange}
+  //    />
+  //    <button type="submit">save</button>
+  //  </form>  
+  //)
+
+  return (
+    <div>
+      {!user && <div>  
+        <h2> Log in to application</h2>
+       {loginForm()}
+        </div>
+      }
+      {user && <div>
+        <h2>blogs</h2>
+        <p>{user.name} logged in</p>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog}/>
+        )}
+      </div>
+      }
+    </div>
+  )
+  }
+
+export default App
