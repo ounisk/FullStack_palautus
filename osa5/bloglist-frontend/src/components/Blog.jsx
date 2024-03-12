3994776f835965d13e1e83bd12fb8dec930e7b3f
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2024-03-12 20:29:03 +0200

    Teht. 5.7, step7

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
+import { useState } from 'react'
+
+const Blog = ({ blog}) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const [visible, setVisible] = useState(false)
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
   <div>
-    {blog.title} {blog.author}
+    <div style={hideWhenVisible}>
+    {blog.title} <button onClick={() => setVisible(true)}>view</button>
+    </div>
+    <div style={showWhenVisible}>
+    {blog.title}  {blog.author} <button onClick={toggleVisibility}>hide</button>
+    <br></br> 
+     {blog.url}  <br></br>{blog.likes} <button onClick={toggleVisibility}>like</button>  <br></br>{blog.user.name} <br></br>
+     </div>
   </div>  
-)
+  
+  </div>
+)}
 
 export default Blog
