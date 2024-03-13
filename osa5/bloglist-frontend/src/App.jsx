470c795b1e6391cc40f0eab48985da294154afd5
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2024-03-13 19:31:50 +0200

    Teht. 5.10, step 10

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -164,8 +164,7 @@ const App = () => {
             <BlogForm createBlog={addBlog}/>
 
         </Togglable>
-
-        {blogs.map(blog =>
+        {blogs.sort((a,b)=> b.likes - a.likes).map(blog =>
           <Blog key={blog.id} blog={blog} addLikes={addLikes}/>
         )}
       </div>
