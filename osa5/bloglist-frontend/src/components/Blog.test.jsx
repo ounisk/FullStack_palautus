0b2c8e4db498512bc9d4b28fe4f5fdf82105a7f2
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2024-03-17 17:28:22 +0200

    Teht. 5.14, step2

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 
@@ -30,5 +31,51 @@ test('renders blog title', () => {
   //const { container } = render(<Blog blog={blog} user={user} />)
   //const div = container.querySelector('.Blog')
   //expect(div).toHaveTextContent('Suomalaisuuden ydintä etsimässä')
-
 })
+
+
+  test('clicking the view button shows url, likes&number and user, ', async () => {
+    const userCurrent = {
+        username:'tikku',
+        name: 'Tiina Teikku',
+        password: 'salasana'
+    } 
+
+  const blog = {
+    title: 'Suomalaisuuden ydintä etsimässä',
+    author: 'Seppo Suominen',
+    url: 'www.suomi.fi',
+    likes: 99,
+    user: {
+        username:'tikku',
+        name: 'Tiina Teikku',
+        password: 'salasana'
+    } 
+  }
+  
+    const mockHandler = vi.fn()
+  
+    render(
+      <Blog blog={blog} user={userCurrent} toggleVisibility={mockHandler} />
+    )
+  
+    const user = userEvent.setup()
+    
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const username = screen.findByText('Tiina Teikku') //getByText('Tiina Teikku')
+    
+    const urldef = screen.findByText('www.suomi.fi')
+    const likes = screen.findByText("likes")
+    const likes_number = screen.findByText("99")
+    //expect(element).toBeDefined()
+    screen.debug()
+    expect(username).toBeDefined()
+    expect(urldef).toBeDefined()
+    expect(likes).toBeDefined()
+    expect(likes_number).toBeDefined()
+
+
+    //expect(mockHandler.mock.calls).toHaveLength(1)   // tällä tulee 0
+  })
