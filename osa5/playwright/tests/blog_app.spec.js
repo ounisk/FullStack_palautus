25482e28247d82bbab1dfd1b9a56fcc1cd64ebf8
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2024-03-26 12:48:58 +0200

    Refactor login tests with helper

--- osa5/playwright/tests/blog_app.spec.js
+++ osa5/playwright/tests/blog_app.spec.js
@@ -1,4 +1,6 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
+
 
 describe('Blog app', () => {
     beforeEach(async ({ page, request }) => {
@@ -28,8 +30,9 @@ test('front page can be opened', async ({ page }) => {
 describe('Login', () => {
     test('succeeds with correct credetials', async ({ page }) => {
         //await page.goto('http://localhost:5174')
-        await page.getByTestId('username').fill('mluukkai')
-        await page.getByTestId('password').fill('salainen')
+        //await page.getByTestId('username').fill('mluukkai')
+        //await page.getByTestId('password').fill('salainen')
+        await loginWith(page, 'mluukkai', 'salainen')
     
         await page.getByRole('button', { name: 'login' }).click()
         await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
@@ -37,8 +40,9 @@ describe('Login', () => {
 
 
     test('fails with wrong credentials', async ({ page }) => {
-        await page.getByTestId('username').fill('mluukkai')
-        await page.getByTestId('password').fill('salkku')
+        //await page.getByTestId('username').fill('mluukkai')
+        //await page.getByTestId('password').fill('salkku')
+        await loginWith(page, 'mluukkai', 'salkku')
     
         await page.getByRole('button', { name: 'login' }).click()
 
